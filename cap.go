6ecf2c02e5038e244538cdec0c3ed80fec7b4506
Faith <atigahfaith44@gmail.com>
2026-05-22 06:27:20 +0100

    Initial commit

--- cap.go
+++ cap.go
@@ -1,38 +1,25 @@
 package main
 
-import "github.com/01-edu/z01"
+import "fmt"
 
-// Notice: No return type here! This matches "want ()"
-func PrintMemory(arr [10]byte) {
-	hexDigits := "0123456789abcdef"
-	output := ""
-
-	// 1. Build the Hex strings
-	for i, b := range arr {
-		output += string(hexDigits[b/16]) + string(hexDigits[b%16])
-		if i == 3 || i == 7 || i == 9 {
-			output += "\n"
-		} else {
-			output += " "
+func IsCapitalized(s string) bool {
+	if s == "" {
+		return false
+	}
+	for i :=0; i<len(s); i++{
+		if s[i] >= 'a' && s[i] <= 'z' && (i==0 || s[i-1]== ' ') {
+			return false
 		}
 	}
+	return true
 
-	// 2. Build the ASCII characters
-	for _, b := range arr {
-		if b >= 32 && b <= 126 {
-			output += string(b)
-		} else {
-			output += "."
-		}
-	}
-	output += "\n"
-
-	// 3. Print the final combined text block letter by letter
-	for _, ch := range output {
-		z01.PrintRune(ch)
-	}
 }
 
 func main() {
-	PrintMemory([10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'})
+	fmt.Println(IsCapitalized("Hello! How are you?")) // false
+	fmt.Println(IsCapitalized("Hello How Are You"))   // true
+	fmt.Println(IsCapitalized("Whats 4this 100K?"))   // true
+	fmt.Println(IsCapitalized("Whatsthis4"))          // true
+	fmt.Println(IsCapitalized("!!!!Whatsthis4"))      // true
+	fmt.Println(IsCapitalized(""))                    // false
 }

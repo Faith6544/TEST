6fb42b27e7b27f58d0f98d87b5c2244554294b4b
Fatigah <atigahfaith44@gmail.com>
2026-05-25 14:20:06 +0100

    rep

--- repeatalpha.go
+++ repeatalpha.go
@@ -1,19 +1,29 @@
-package main
+// Online Go compiler to run Golang program online
+// Print "Start small. Ship something." message
 
+package main
 import "fmt"
 
 func RepeatAlpha(s string) string {
-	result := ""
-	for i, ch := range s {
-
-		for j := 0; j < i+1; j++ {
-			result += string(ch)
-		}
-
-	}
-	return result
+    res:= ""
+    for _, ch := range s {
+        if ch >='a' && ch <= 'z' {
+            count := int( ch -'a' + 1)
+            for i:= 0 ; i < count ; i++{
+                res +=string(ch)
+            }
+        } else if ch >='A' && ch <= 'Z' {
+            count := int( ch -'A' + 1)
+            for i:= 0 ; i < count ; i++{
+                res +=string(ch)
+            }
+        } else {
+            res+= string(ch)
+        }
+    }
+    return res
 }
-func main() {
+ func main() {
 	fmt.Println(RepeatAlpha("abc"))
 	fmt.Println(RepeatAlpha("Choumi."))
 	fmt.Println(RepeatAlpha(""))
